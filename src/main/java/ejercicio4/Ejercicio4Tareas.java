17f9767b09457d278c19214dc274248040486f7e
Nowels033 <noeldomse@gmail.com>
2024-02-06 14:04:20 +0100

    ASIGNACION DE TAREAS

---
+++ src/main/java/ejercicio4/Ejercicio4Tareas.java
@@ -0,0 +1,65 @@
+package ejercicio4;
+
+public class Ejercicio4Tareas {
+
+	
+//	
+//	
+//	¡¡¡¡IMPORTANTE!!!!
+//	TENED EN CUENTA QUE DEBEIS IMPLICAROS EN QUE EL TRABAJO SE PUEDA REALIZAR EN EQUIPO, PARA ELLO PODEIS ORGANIZAR LOS TIEMPOS DE ENTREGA DE CADA TAREA.
+//
+//	EN CASO QUE NO HAYA IMPLICACIÖN POR PARTE DE UN COMPONENTE, EL RESTO PODEIS IMPLEMENTAR VUESTRAS TAREAS Y SUBIRLAS AUNQUE SEA EN MËTODOS A PARTE SI DEPENDEIS DE LAS DE OTROS
+//
+//	LA NOTA ES INDIVIDUAL AL TRABAJO DE CADA UNO, SE TENDRÁ EN CUENTA EL EQUIPO QUE HAYA PODIDO TRABAJAR EN UNA DINÁMICA GRUPAL EFECTIVA ENTRE TODOS SUS COMPONENTES.
+//
+//	Se quiere desarrollar una sencilla aplicación para la gestión de un CajeroEletronico.
+//
+//	LAS  DISTRIBUCIÓN ENTRE COMPONENTES DEL EQUIPO DE LAS TAREAS SIGUIENTES SON UNA GUÍA, LAS PODEIS DISTRIBUIR COMO VEAIS MÁS CONVENIENTE.
+//	PERO HAY QUE GENERAR UNA BRANCH POR CADA TAREA (Salvo para las clonaciones y la creación del repositorio)
+//
+//	COORDINADOR//NOEL
+//	- Tarea 1 crear un repositorio en gitHub con el nombre EED24U4A4EquipoXCE  (invitar a los compañeros y al profesor)
+//	  Crear un proyecto maven y subirlo al repositorio.
+//	- Tarea 2(1) crear Clase principal CajeroEletronico con un método main de ejecución que muestre un menú de estados solicitando:
+//	   - Identificador de cliente
+//	   - Password de cliente
+//	   tras lo que mostrará un mensaje saludando al identificador.
+//	- Tarea 3(2) Implementar el método Transferir importe en Cliente e inclúyelo en CajeroElectronico
+//
+//
+//	PROGRAMADOR 1(RAQUEL)
+//	- Tarea 1 clonar repositorio
+//	- Tarea 2(3) crear Clase Cliente con los atributos identificador, password
+//	    crear un registro de clientes en la clase CajeroEletronico con 5 clientes diferentes
+//	    modificar la ejecución del menu para que compruebe si el identificador introducido existe y después si la password es correcta.
+//	   En caso erróneo se debe mostrar y volverá a empezar:
+//	     - Si el usuario no está en el registro: "Error: el usuario no existe"
+//	     - Si la contraseña no es correcta: "Error: credenciales incorrectas"
+//	- Tarea 3 (4)Implementar el método Ingresar importe en Cliente e inclúyelo en CajeroElectronico
+//
+//
+//	PROGRAMADOR 2(PABLO)
+//	- Tarea 1 clonar repositorio
+//	- Tarea 2 (5)crear un menú de operaciones en CajeroElectronico tras haber realizado un logueo satisfactorio que muestre:
+//	    A: Mostrar saldo actual
+//	    B: Ingresar importe
+//	    C: Obtener importe
+//	    D: Transferir importe
+//	    S: Salir (volverá al menú de introducción de claves)
+//	   actualizar la clase cliente para que tenga un atributo monedero.  El monedero debe ser generado en el constructor como un número aleatorio entre 1000 y 20000.
+//	- Tarea 3 (6)Implementar el método Obtener importe en Cliente e inclúyelo en CajeroElectronico
+//
+//	SE HA DE GENERAR EL CÓDIGO DE TEST EN JUNIT PARA LAS CLASES O MÉTODOS REALIZADOS EN CADA TAREA
+//	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
